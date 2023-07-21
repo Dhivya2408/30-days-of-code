82bebc33dc2fe37ffd098638442b364604f5938d
Dhivya2408 <126559133+Dhivya2408@users.noreply.github.com>
2023-07-21 23:05:13 +0530

    Update anagram.py

--- anagram.py
+++ anagram.py
@@ -1,3 +1,26 @@
+Input:
+
+Input 1: 1st string
+Input 2: 2nd string
+
+Output:
+(If they are anagrams, the function will return ‘yes’. Otherwise, it will return ‘no’.)
+
+Example
+
+Input 1: Listen
+Input 2: Silent
+
+Output:
+Yes
+
+Explanation
+
+Listen and Silent are anagrams (an anagram is a word formed by rearranging the letters of the other word).
+
+
+output:
+
 s1=input()
 s2=input()
 print(s1)
