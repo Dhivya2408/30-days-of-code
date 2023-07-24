3a032ed1a7a406590a8d7272226d72d86b2ee4c3
Dhivya2408 <126559133+Dhivya2408@users.noreply.github.com>
2023-07-24 12:37:30 +0530

    Create frequency.py

---
+++ frequency.py
@@ -0,0 +1,18 @@
+Write a program to find the frequency of a user-given character in a string.
+
+
+
+n=input()
+total={}
+for i in n:
+    if i in total:
+        total[i]+=1
+    else: 
+        total[i]=1
+print("count",str(total))
+
+
+output:
+
+dhivyaaa
+count {'d':1,'h':1,'i':1,'v':1,'y':1,'a':3}
